f9f99252f08236ee04634515e3caefefef093a6e
Piyush Lodaya <ppiyush13@gmail.com>
2019-09-26 00:20:52 +0530

    remove unwanted import from index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {shallow, mount} from 'enzyme'
+import {mount} from 'enzyme'
 import 'jest-styled-components'
 import {FlexBox, FlexItem, __RewireAPI__} from '../src'
 
