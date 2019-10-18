947c1f36680eae12d02ab01ce619c614ef40eedf
Piyush Lodaya <ppiyush13@gmail.com>
2019-10-18 19:46:51 +0530

    Adding dist source maps

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonJs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import pkg from './package.json';
 
 export default [
@@ -11,28 +12,29 @@ export default [
             {
                 file: pkg.module,
                 format: 'esm',
-                sourcemap: false,
+				sourcemap: true,
             },
             {
                 file: pkg.main,
                 format: 'cjs',
-                sourcemap: false,
+                sourcemap: true,
                 exports: 'named',
                 name: 'reactFlexbox',
             },
         ],
         external: Object.keys(pkg.peerDependencies),
         plugins: [
+			sourcemaps(),
+            nodeResolve(),
             babel({
                 babelHelpers: 'bundled',
                 skipBabelHelpersCheck: true,
                 exclude: 'node_modules/**',
                 compact: false,
             }),
-            nodeResolve(),
             commonJs(),
             terser({
-                sourcemap: false,
+                sourcemap: true,
             }),
         ],
     },
