4a7b1d34852a61666252c2fc090503f93a55f179
ppiyush13 <ppiyush13@gmail.com>
2021-04-04 20:41:26 +0530

    add next.js example

---
+++ examples/next-js/pages/_app.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        font-family: Segoe UI;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`
+
+const theme = {
+    colors: {
+        primary: '#0070f3',
+    },
+}
+
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
+}
