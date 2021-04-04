ec168505db78ee844e94c3cbc885c3e73af139ed
ppiyush13 <ppiyush13@gmail.com>
2021-04-04 21:41:46 +0530

    add gatsby example

---
+++ examples/gatsby/gatsby-ssr.js
@@ -0,0 +1,5 @@
+exports.onRenderBody = ({ setBodyAttributes }) => {
+    setBodyAttributes({
+        className: 'flex-gap-not-supported'
+    });
+};
---
+++ examples/gatsby/src/pages/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Box, FlexBox, FlexItem } from 'react-styled-flex';
+import Layout from '../components/layout';
+
+const App = () => {
+    return (
+        <Layout>
+            <div>
+                <Header as={'header'} center >
+                    Gatsby
+                </Header>
+                <FlexBox column gap={32} padding={'2rem'}>
+                    <NormalChild >Normal Child</NormalChild>
+                    <Box
+                        padding={'2px 1rem'}
+                        border={'1px dashed crimson'}
+                    >
+                        Box Child
+                    </Box>
+                    <FlexItem 
+                        box 
+                        gap={'10px'}
+                        padding={'16px'}
+                        border={'1px dashed darkgreen'}
+                    >
+                        <Box
+                            padding={'2px 1rem'}
+                            border={'1px dashed darkseagreen'}
+                        >
+                            Nested child 1
+                        </Box>
+                        <Box
+                            padding={'2px 1rem'}
+                            border={'1px dashed darkseagreen'}
+                        >
+                            Nested child 2
+                        </Box>
+                    </FlexItem>
+                </FlexBox>
+            </div>
+        </Layout>
+    );
+};
+
+const Header = styled(FlexBox)`
+    height: 4rem;
+    background-color: #663399;
+    color: white;
+`;
+
+const NormalChild = styled.div`
+    border: 1px dashed blue;
+    padding: 2px 1rem;
+`;
+
+export default App;
