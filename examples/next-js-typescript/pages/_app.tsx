c0ab273e19d42e570a133b53d0ee8d4f3e9632b1
ppiyush13 <ppiyush13@gmail.com>
2021-04-04 21:02:11 +0530

    add example for next.js with typescript

---
+++ examples/next-js-typescript/pages/_app.tsx
@@ -0,0 +1,30 @@
+import type { AppProps  } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        font-family: Segoe UI;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`
+
+const theme = {
+    colors: {
+        primary: '#0070f3',
+    },
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
+}
