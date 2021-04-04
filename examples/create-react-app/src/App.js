7e9259e6807613f38b99bbbacdde67f3e46421eb
ppiyush13 <ppiyush13@gmail.com>
2021-04-04 19:02:01 +0530

    examples using create-react-app

---
+++ examples/create-react-app/src/App.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import { Box, FlexBox, FlexItem } from 'react-styled-flex';
+import logo from './logo.svg';
+
+export const App = () => {
+    return (
+        <div>
+            <Header as={'header'} center >
+                <ReactLogo src={logo} alt="logo" />
+            </Header>
+            <FlexBox column gap={32} padding={'2rem'}>
+                <NormalChild >Normal Child</NormalChild>
+                <Box
+                    padding={'2px 1rem'}
+                    border={'1px dashed crimson'}
+                >
+                    Box Child
+                </Box>
+                <FlexItem 
+                    box 
+                    gap={'10px'}
+                    padding={'16px'}
+                    border={'1px dashed darkgreen'}
+                >
+                    <Box
+                        padding={'2px 1rem'}
+                        border={'1px dashed darkseagreen'}
+                    >
+                        Nested child 1
+                    </Box>
+                    <Box
+                        padding={'2px 1rem'}
+                        border={'1px dashed darkseagreen'}
+                    >
+                        Nested child 2
+                    </Box>
+                </FlexItem>
+            </FlexBox>
+        </div>
+    );
+};
+
+const Header = styled(FlexBox).withConfig({ displayName: 'Header' })`
+    height: 50px;
+    background-color: #20232a;
+`;
+
+const ReactLogo = styled.img`
+    height: 32px;
+`;
+
+const NormalChild = styled.div`
+    border: 1px dashed blue;
+    padding: 2px 1rem;
+`;
