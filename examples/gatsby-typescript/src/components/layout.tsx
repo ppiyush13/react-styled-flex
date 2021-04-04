a3e5fb95a3c18ecb93746b8bb42c8447fd69fc3e
ppiyush13 <ppiyush13@gmail.com>
2021-04-04 21:55:58 +0530

    add example for gatsby-typescript

---
+++ examples/gatsby-typescript/src/components/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        font-size: 16px;
+        font-family: Segoe UI;
+    }
+
+    * {
+        padding: 0;
+        margin: 0;
+    }
+`;
+
+export default function Layout({ children }: { children: React.ReactElement }) {
+    return (
+        <React.Fragment>
+            <GlobalStyle />
+            {children}
+        </React.Fragment>
+    )
+}
