6ec644bb992afe9c61f50f075ed9f5dfe8fdddec
ppiyush13 <ppiyush13@gmail.com>
2021-04-04 20:41:26 +0530

    add next.js example

---
+++ examples/next-js/pages/index.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+import { Box, FlexBox, FlexItem } from 'react-styled-flex';
+
+const App = () => {
+    return (
+        <div>
+            <Header as={'header'} center >
+                Next.js
+            </Header>
+            <FlexBox column gap={32} padding={'2rem'}>
+                <NormalChild >Normal Child</NormalChild>
+                <Box
+                    padding={'2px 1rem'}
+                    border={'1px dashed crimson'}
+                >
+                    Box Child
+                </Box>
+                <FlexItem 
+                    box 
+                    gap={'10px'}
+                    padding={'16px'}
+                    border={'1px dashed darkgreen'}
+                >
+                    <Box
+                        padding={'2px 1rem'}
+                        border={'1px dashed darkseagreen'}
+                    >
+                        Nested child 1
+                    </Box>
+                    <Box
+                        padding={'2px 1rem'}
+                        border={'1px dashed darkseagreen'}
+                    >
+                        Nested child 2
+                    </Box>
+                </FlexItem>
+            </FlexBox>
+        </div>
+    );
+};
+
+const Header = styled(FlexBox)`
+    height: 4rem;
+    background-color: #0070f3;
+    color: white;
+`;
+
+const ReactLogo = styled.img`
+    height: 3rem;
+    animation: App-logo-spin infinite 20s linear;
+    @keyframes App-logo-spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+`;
+
+const NormalChild = styled.div`
+    border: 1px dashed blue;
+    padding: 2px 1rem;
+`;
+
+export default App;
