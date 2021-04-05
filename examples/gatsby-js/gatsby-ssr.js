6e7b9a7584ae8a87d9c12f69fab779cd97083850
ppiyush13 <ppiyush13@gmail.com>
2021-04-05 22:35:57 +0530

    improving gatsby examples

--- examples/gatsby-js/gatsby-ssr.js
+++ examples/gatsby-js/gatsby-ssr.js
@@ -1,5 +1,13 @@
-exports.onRenderBody = ({ setBodyAttributes }) => {
+const React = require("react")
+
+exports.onRenderBody = ({ setBodyAttributes, setHeadComponents }) => {
     setBodyAttributes({
         className: 'flex-gap-not-supported',
     });
+    setHeadComponents([
+        <React.Fragment key={'head'}>
+            <title>react-styled-flex using Gatsby</title>
+            <link rel="icon" href="https://www.gatsbyjs.com/favicon-32x32.png" type="image/png"></link>
+        </React.Fragment>,
+    ])
 };
