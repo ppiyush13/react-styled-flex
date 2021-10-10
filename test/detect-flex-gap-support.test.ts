af7400f638f03a42e34ed54d70015b68c1d28816
Piyush Lodaya <ppiyush13@gmail.com>
2021-10-10 18:32:30 +0530

    Fix lint issues

--- test/detect-flex-gap-support.test.ts
+++ test/detect-flex-gap-support.test.ts
@@ -1,5 +1,4 @@
 import mockComputedStyles from './util/mock-computed-styles';
-import mockDomContentLoaded from './util/mock-dom-content-loaded';
 import { FlexGapNotSupportedClassName, FlexGapSupportedClassName } from '../src/constants';
 
 describe('testing detect-flex-gap-support module', () => {
@@ -22,31 +21,5 @@ describe('testing detect-flex-gap-support module', () => {
 
         /* cleanup */
         restoreComputedStyles();
-    });
-});
-
-describe.only('testing scenarios when DOM is not loaded yet', () => {
-    beforeEach(() => { document.body.className = ''; });
-
-    it('should set class on body when DOM content is loaded', async () => {
-        const { waitForDomContentToLoad, loadDom, restore } = mockDomContentLoaded();
-
-        /* require actual module */
-        jest.requireActual('../src/detect-flex-gap-support');
-
-        /* assert that class is not yet set */
-        expect(document.body.classList.toString()).toBe('');
-
-        /** mark document.readyState as complete and trigger DOMContentLoaded event */
-        loadDom();
-
-        /** wait until DOMContentLoaded event is raised and handled */
-        await waitForDomContentToLoad();
-
-        /** assert that body has class name */
-        expect(document.body.classList.toString()).toBe(FlexGapNotSupportedClassName);
-
-        /** restore domContentLoaded mock */
-        restore();
     });
 });
